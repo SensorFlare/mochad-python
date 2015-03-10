9ee27c3b3a5f7fbddc88fe31228078e9050049cc
Dimitrios Amaxilatis <amaxilat@cti.gr>
2015-03-10 12:57:08 +0000

    first commit of a working mochad wrapper for sensorflare

---
+++ mochad_python.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+__author__ = 'amaxilatis'
+
+# required for logging
+import logging
+# required for rabbitmq connections
+import pika
+# required for mochad communication
+import hautomation_x10.cmds as mochad
+
+from properties import *
+
+
+logging.basicConfig()
+
+# create the rabbitmq exchange and queue names to communicate with sensorflare
+send_exchange = "mochad-" + username + "-send"
+commands_exchange = "mochad-" + username + "-commands"
+commands_queue = "mochad-" + username + "-commands"
+
+print " [x] send_queue: ", send_exchange
+print " [x] commands_queue: ", commands_queue
+print " [x] commands_exchange: ", commands_exchange
+
+# sensorflare rabbitmq server
+sensorflareHost = 'mochad.sensorflare.com'
+# create the credentials to communicate with rabbitmq
+sensorflareCredentials = pika.credentials.PlainCredentials(username, password)
+
+# function that connects to rabbitmq and listens for commands
+def create_connection(host, credentials):
+    # create a blocking connection with pika
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host, credentials=credentials))
+    # open a channel for rabbitmq
+    channel = connection.channel()
+    print " [x] connected to 'mochad.sensorflare.com'"
+    # declare the commands exchange if missing
+    channel.exchange_declare(exchange=commands_exchange, type='topic', durable=True, internal=False, auto_delete=False)
+    # bind our queue to the exchange
+    channel.queue_bind(exchange=commands_exchange, queue=commands_queue)
+
+    def rabbitSend(message):
+        try:
+            channel.basic_publish(exchange=send_exchange, routing_key=send_exchange, body=message)
+        except:
+            print ' [x] error sending back message'
+
+    def callback(ch, method, properties, body):
+        print " [x] %r %r %r %r" % (body, ch, method, properties)
+        body_parts = body.split(" ")
+        if len(body_parts) == 3:
+            target = body_parts[1]
+            command = body_parts[2]
+            mochad.pl_switch(target, command)
+            print ' [x] switch command sent'
+            rabbitSend('sent: ' + body + '\n')
+
+        if len(body_parts) == 4:
+            target = body_parts[1]
+            command = body_parts[3]
+            mochad.pl_dim(target, command)
+            print ' [x] dim command sent'
+            rabbitSend('sent: ' + body + '\n')
+
+    # set the consume callback
+    channel.basic_consume(callback, queue=commands_queue, no_ack=True)
+
+    # start waiting for the commands
+    print ' [*] Waiting for commands. To exit press CTRL+C'
+
+    # send a connect message to sensorflare
+    rabbitSend('connected\n')
+
+    # start the blocking consume
+    try:
+        channel.start_consuming()
+    except:
+        print ' [*] connection was closed!'
+        print ' [x] reconnecting in 5 seconds...'
+        import time
+
+        time.sleep(5)
+        print ' [x] reconnecting...'
+        create_connection(host, credentials)
+
+# start the first connection
+create_connection(sensorflareHost, sensorflareCredentials)
