c10f4163358f8c5da0b9f6c5f96c5d812598cb33
Dimitrios Amaxilatis <amaxilat@cti.gr>
2015-03-11 12:52:08 +0000

    remove no longer needed dependency

--- tools/install.sh
+++ tools/install.sh
@@ -6,5 +6,3 @@ easy_install pip
 
 # pika library for rabbitmq
 pip install pika==0.9.8
-# hautomation_x10 library for communication with mochad
-pip install -e git+https://github.com/jpardobl/hautomation_x10.git#egg=hautomation_x10
